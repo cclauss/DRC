37d7ea11053f8f391e2a511519c0c910c170a6ed
cclauss <cclauss@bluewin.ch>
2018-09-08 07:45:10 +0000

    \ line termination not required inside (), [], {}, etc. (#9)
    Also “A” + “b” == “Ab”

--- download_reddit_comments.py
+++ download_reddit_comments.py
@@ -16,26 +16,26 @@ while freq not in ('day', 'week', 'month', 'year'):
 	"Please enter a comment frequency. Choose one of "
 	"these:\n\nday\nweek\nmonth\nyear").strip().lower()
 	
-limit = console.input_alert("DRC by GoDzM4TT3O", \
-                            "How many comments would you like to download? "\
+limit = console.input_alert("DRC by GoDzM4TT3O", 
+                            "How many comments would you like to download? "
                             + "[Example: 50]\nMUST BE 10 or more.", "10")
 
 nums = re.compile("^[\-]?[1-9][0-9]*\.?[0-9]+$")
 isnum = re.match(nums, limit)
 if not isnum:
-	console.alert("DRC by GoDzM4TT3O", \
-	"Please enter a numeric comment limit."\
-	+ "For example, if I want to download my last 10 comments this week, "\
-	+ "I will choose 'week' as comment frequency, and 10 as comment limit.")
+	console.alert("DRC by GoDzM4TT3O",
+	"Please enter a numeric comment limit."
+	"For example, if I want to download my last 10 comments this week, "\
+	"I will choose 'week' as comment frequency, and 10 as comment limit.")
 	
 if not limit:
-	console.alert("DRC by GoDzM4TT3O", \
-	"Please enter a numeric comment limit."\
-	+ "For example, if I want to download my last 10 comments this week, "\
-	+ "I will choose 'week' as comment frequency, and 10 as comment limit.")
+	console.alert("DRC by GoDzM4TT3O",
+	"Please enter a numeric comment limit."
+	"For example, if I want to download my last 10 comments this week, "
+	"I will choose 'week' as comment frequency, and 10 as comment limit.")
 	
 rawu = console.input_alert("DRC by GoDzM4TT3O", \
-                           "Please enter a valid Reddit username of your choice to download their "\
+                           "Please enter a valid Reddit username of your choice to download their "
                            + limit + " comments with the frequency: " + freq)
 user = rawu.replace(" ", "")
 
@@ -44,16 +44,16 @@ user = rawu.replace(" ", "")
 # the program will fetch random usernames!
 
 if not user:
-	console.alert("DRC by GoDzM4TT3O", \
+	console.alert("DRC by GoDzM4TT3O",
 	"Please enter a valid username.")
 	
-subreddit = console.input_alert("DRC by GoDzM4TT3O", \
-                                "[OPTIONAL] Enter a subreddit you'd like to download comments from."\
-                                + "\nIf no subreddit is defined, you'll only download "\
-                                + limit + " comments made by " + \
-                                user + " with this frequency: " + \
-                                freq + " in every subreddit they commented."\
-                                "\nIn alternative, if a subreddit is provided, you'll download "\
+subreddit = console.input_alert("DRC by GoDzM4TT3O",
+                                "[OPTIONAL] Enter a subreddit you'd like to download comments from."
+                                "\nIf no subreddit is defined, you'll only download "
+                                + limit + " comments made by "
+                                + user + " with this frequency: "
+                                freq + " in every subreddit they commented."
+                                "\nIn alternative, if a subreddit is provided, you'll download "
                                 + user + "'s comments in the provided subreddit, if the user and the subreddit are valid.")
 
 apiurl = "https://api.pushshift.io/reddit/comment/search?author=" + user + "&frequency=" + freq + "&limit=" + limit
@@ -62,7 +62,7 @@ if subreddit:
 	
 data = requests.get(apiurl).json()
 if not data.get('data'):
-	print(("\nDRC by GoDzM4TT3O\n\nERROR: The specified user", \
+	print(("\nDRC by GoDzM4TT3O\n\nERROR: The specified user",
 	user, "doesn't exist."))
 	
 ## Made  by  GoDzM4TT3O & cclauss
@@ -71,7 +71,7 @@ for element in data['data']:
 
 	author = element['author']
 	contentold = element[u'body']
-	content = unicodedata.normalize('NFKD',  \
+	content = unicodedata.normalize('NFKD',
 	contentold).encode('ascii', 'ignore').decode()
 	permalink = element['permalink']
 	upvotes = element['score']
@@ -80,14 +80,14 @@ for element in data['data']:
 	unix_date = element['retrieved_on']
 	date = time.strftime('%d-%m-%Y %H:%M:%S', time.localtime(unix_date))
 	
-	print("\nAuthor:\n", \
-	author, "\n\nComment content:\n", \
-	content, "\n\nPermalink:\n", \
-	permalink, "\n\nUpvotes:\n", \
-	upvotes, "\n\nIs sticky post:\n", \
-	issticky, "\nSubreddit:\n", \
-	sub, "\n\nPosted on:\nUnix date:\n", \
-	unix_date, "\nDate (DD/MM/YYYY):\n", \
-	date, "\n\n--------------------\n\n", \
+	print("\nAuthor:\n",
+	author, "\n\nComment content:\n",
+	content, "\n\nPermalink:\n",
+	permalink, "\n\nUpvotes:\n",
+	upvotes, "\n\nIs sticky post:\n",
+	issticky, "\nSubreddit:\n",
+	sub, "\n\nPosted on:\nUnix date:\n",
+	unix_date, "\nDate (DD/MM/YYYY):\n",
+	date, "\n\n--------------------\n\n",
 	file=open(outname, "a"))
 # Made by GoDzM4TT3O & cclauss
